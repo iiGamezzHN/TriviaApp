ff21cd6b1f99b1a3bb5b8b7529959796348e1231
[iiGamezzHN] <[davidarisz@gmail.com]>
2018-12-10 11:44:01 +0100

    Initialize project with 2 activities

---
+++ app/src/main/java/com/example/cpuga/triviaapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.cpuga.triviaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void toQuestion(View view) {
+
+    }
+}
